b8e406e14d5ac6d618bdfe171691772b6c750ce2
Harshal Bhangale <buddyharshal2751@gmail.com>
2025-02-09 13:31:17 +0530

    Replaced frontend with fresh Nextjs

--- app/library/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "@/components/layout"
-
-export default function LibraryLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <Layout>{children}</Layout>
-}
--- app/transactions/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "@/components/layout"
-
-export default function TransactionsLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <Layout>{children}</Layout>
-}
